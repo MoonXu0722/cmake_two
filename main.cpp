72679eb8d9d3b41d5fc0baf5075820d86a6ab2f3
MoonXu0722 <xpylovely520@gmail.com>
2020-06-25 02:33:41 +0800

    20200625

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿// subdir.cpp: 定义应用程序的入口点。
+//
+
+#include "main.h"
+#include "math\add.h"
+using namespace std;
+
+int main()
+{
+	cout << add<int>(1, 3) << endl;
+	cout << add<string>("Hello ", "LiuYuan") << endl;
+	cout << add<double>(1.3, 2.8) << endl;
+	return 0;
+}
+
+
---
+++ math/add.h
@@ -0,0 +1,10 @@
+
+#ifndef ADD_H
+#define ADD_H
+
+template<typename T>
+T add(const T& x, const T& y)
+{
+	return x + y;
+}
+#endif
